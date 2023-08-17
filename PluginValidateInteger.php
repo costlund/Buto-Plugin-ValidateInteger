ad93712c12b47f237befba29d5db53d93771ba1f
Claes Östlund <claes@stenbergit.se>
2023-08-17 17:41:32 +0200

    1.4.0

--- PluginValidateInteger.php
+++ PluginValidateInteger.php
@@ -7,16 +7,16 @@ class PluginValidateInteger{
     $this->i18n->setPath('/plugin/validate/integer/i18n');
   }
   public function validate_integer($field, $form, $data = array()){
-    if(wfArray::get($form, "items/$field/is_valid") && strlen(wfArray::get($form, "items/$field/post_value"))){ // Only if valid and has data.
+    if(wfArray::get($form, "items/$field/is_valid") && wfPhpfunc::strlen(wfArray::get($form, "items/$field/post_value"))){ // Only if valid and has data.
       wfPlugin::includeonce('wf/array');
       $data = new PluginWfArray($data);
       if (!$this->is_integer(wfArray::get($form, "items/$field/post_value"))) {
         $form = wfArray::set($form, "items/$field/is_valid", false);
         $form = wfArray::set($form, "items/$field/errors/", $this->i18n->translateFromTheme('?label is not an integer!', array('?label' => wfArray::get($form, "items/$field/label"))));
-      }elseif(strlen($data->get('min')) && (int)wfArray::get($form, "items/$field/post_value") < $data->get('min')){
+      }elseif(wfPhpfunc::strlen($data->get('min')) && (int)wfArray::get($form, "items/$field/post_value") < $data->get('min')){
         $form = wfArray::set($form, "items/$field/is_valid", false);
         $form = wfArray::set($form, "items/$field/errors/", $this->i18n->translateFromTheme('?label can not be less then ?min!', array('?label' => wfArray::get($form, "items/$field/label"), '?min' => $data->get('min'))));
-      }elseif(strlen($data->get('max')) && (int)wfArray::get($form, "items/$field/post_value") > $data->get('max')){
+      }elseif(wfPhpfunc::strlen($data->get('max')) && (int)wfArray::get($form, "items/$field/post_value") > $data->get('max')){
         $form = wfArray::set($form, "items/$field/is_valid", false);
         $form = wfArray::set($form, "items/$field/errors/", $this->i18n->translateFromTheme('?label can not be greater then ?max!', array('?label' => wfArray::get($form, "items/$field/label"), '?max' => $data->get('max'))));
       }
@@ -24,7 +24,7 @@ class PluginValidateInteger{
     return $form;   
   }
   public function is_integer($num){
-    if(preg_match('/^\d+$/',str_replace('-', '', $num))){
+    if(preg_match('/^\d+$/',wfPhpfunc::str_replace('-', '', $num))){
       return true;
     }else{
       return false;
